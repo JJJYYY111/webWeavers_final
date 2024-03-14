db75ad23cf5eaa8eac0fe6daf1ff0fadf4587a45
leesin <a25611640@gmail.com>
2024-03-14 14:08:37 +0900

    [upload] 초기파일

---
+++ src/main/java/com/webWeavers/weaveGlow/WeaveGlowApplication.java
@@ -0,0 +1,13 @@
+package com.webWeavers.weaveGlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaveGlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaveGlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webWeavers/weaveGlow/WeaveGlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webWeavers.weaveGlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaveGlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
