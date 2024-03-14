8115e294481a5a88bf192a93554c00ef5ed4e883
장예원 <hoihoigd@naver.com>
2024-03-14 20:38:04 +0900

    [test]

--- src/main/java/com/webWeavers/weaveGlow/biz/product/ProductDAO.java
+++ src/main/java/com/webWeavers/weaveGlow/biz/product/ProductDAO.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
+// Test 주석
 @Repository("productDAO")
 public class ProductDAO {
 
