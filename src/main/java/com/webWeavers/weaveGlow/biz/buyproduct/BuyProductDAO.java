a739d5bcc064524b7e5a7cc0fc0d004126afaf51
hoihoigd <hoihoigd@naver.com>
2024-03-20 14:28:39 +0900

    [update] BuyProduct 이쁘게 보이기려고 엔터수정뿐임

--- src/main/java/com/webWeavers/weaveGlow/biz/buyproduct/BuyProductDAO.java
+++ src/main/java/com/webWeavers/weaveGlow/biz/buyproduct/BuyProductDAO.java
@@ -16,9 +16,25 @@ public class BuyProductDAO {
 	private JdbcTemplate jdbcTemplate;
 	
 	// 구매목록페이지
-	private static final String SELECTALL_CHECKOUTLIST = "SELECT B.BUYPRODUCT_PK, B.BUYPRODUCT_CNT, S.SERIAL_PK, S.SERIAL_REGDATE, S.SERIAL_DELIVERYADDRESS, P.PRODUCT_NAME, P.PRODUCT_PRICE, P.PRODUCT_IMG, P.PRODUCT_PK, IF(R.REVIEW_PK IS NOT NULL, 1, 0) REVIEWCHECK FROM BUYPRODUCT B JOIN SERIAL S ON(B.SERIAL_PK= S.SERIAL_PK) JOIN PRODUCT P ON(B.PRODUCT_PK=P.PRODUCT_PK) LEFT JOIN REVIEW R ON (B.BUYPRODUCT_PK = R.BUYPRODUCT_PK) WHERE S.MEMBER_ID=? ORDER BY B.SERIAL_PK DESC";
+	private static final String SELECTALL_CHECKOUTLIST = "SELECT\r\n"
+			+ "	S.SERIAL_PK, S.SERIAL_REGDATE, S.SERIAL_DELIVERYADDRESS,\r\n"
+			+ "	B.BUYPRODUCT_PK, B.BUYPRODUCT_CNT,\r\n"
+			+ "	P.PRODUCT_PK, P.PRODUCT_PRICE, P.PRODUCT_IMG, P.PRODUCT_NAME,\r\n"
+			+ "	IF(R.REVIEW_PK IS NOT NULL, 1, 0) REVIEWCHECK\r\n"
+			+ "FROM SERIAL S\r\n"
+			+ "LEFT JOIN BUYPRODUCT B ON S.SERIAL_PK = B.SERIAL_PK\r\n"
+			+ "LEFT JOIN PRODUCT P ON B.PRODUCT_PK = P.PRODUCT_PK\r\n"
+			+ "LEFT JOIN REVIEW R ON B.BUYPRODUCT_PK = R.BUYPRODUCT_PK\r\n"
+			+ "WHERE S.MEMBER_ID = ?\r\n"
+			+ "ORDER BY S.SERIAL_PK DESC";
 	// 구매완료페이지_최근구매내역
-	private static final String SELECTALL_CHECKOUTSUCCESS = "SELECT B.BUYPRODUCT_CNT, S.SERIAL_PK, S.SERIAL_REGDATE, P.PRODUCT_NAME, P.PRODUCT_PRICE, P.PRODUCT_IMG, P.PRODUCT_PK FROM SERIAL S JOIN BUYPRODUCT B ON(B.SERIAL_PK= S.SERIAL_PK) JOIN PRODUCT P ON(B.PRODUCT_PK=P.PRODUCT_PK) WHERE S.MEMBER_ID= ? AND S.SERIAL_PK = (SELECT MAX(SERIAL_PK) FROM SERIAL)";
+	private static final String SELECTALL_CHECKOUTSUCCESS = "SELECT\r\n"
+			+ "	S.SERIAL_PK, S.SERIAL_REGDATE, B.BUYPRODUCT_PK, B.BUYPRODUCT_CNT,\r\n"
+			+ "	P.PRODUCT_PK, P.PRODUCT_NAME, P.PRODUCT_PRICE, P.PRODUCT_IMG\r\n"
+			+ "FROM SERIAL S\r\n"
+			+ "JOIN BUYPRODUCT B ON B.SERIAL_PK = S.SERIAL_PK\r\n"
+			+ "JOIN PRODUCT P ON B.PRODUCT_PK = P.PRODUCT_PK\r\n"
+			+ "WHERE S.MEMBER_ID = ? AND S.SERIAL_PK = (SELECT MAX(SERIAL_PK) FROM SERIAL)";
 	// ?
 	private static final String SELECTONE = "SELECT B.BUYPRODUCT_CNT, S.SERIAL_PK, S.SERIAL_REGDATE, P.PRODUCT_NAME, P.PRODUCT_PRICE, P.PRODUCT_IMG, P.PRODUCT_PK FROM BUYPRODUCT B JOIN SERIAL S ON B.SERIAL_PK=S.SERIAL_PK JOIN PRODUCT P ON B.PRODUCT_PK=P.PRODUCT_PK WHERE B.BUYPRODUCT_PK = ?";
 	// 구매상품추가
@@ -84,15 +100,15 @@ class BuyProductRowMapper1 implements RowMapper<BuyProductDTO> {
 	@Override
 	public BuyProductDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
 		BuyProductDTO data = new BuyProductDTO();
-		data.setBuyProductPK(rs.getInt("BUYPRODUCT_PK"));
 		data.setSerialPK(rs.getInt("SERIAL_PK"));
 		data.setSerialRegdate(rs.getDate("SERIAL_REGDATE"));
 		data.setSerialDeliveryAddress(rs.getString("SERIAL_DELIVERYADDRESS"));
+		data.setBuyProductPK(rs.getInt("BUYPRODUCT_PK"));
+		data.setBuyProductCnt(rs.getInt("BUYPRODUCT_CNT"));
+		data.setProductPK(rs.getInt("PRODUCT_PK"));
 		data.setProductName(rs.getString("PRODUCT_NAME"));
 		data.setProductPrice(rs.getInt("PRODUCT_PRICE"));
 		data.setProductImg(rs.getString("PRODUCT_IMG"));
-		data.setProductPK(rs.getInt("PRODUCT_PK"));
-		data.setBuyProductCnt(rs.getInt("BUYPRODUCT_CNT"));
 		data.setReviewCheck(rs.getInt("REVIEWCHECK"));
 		return data;
 	}
@@ -104,11 +120,12 @@ class BuyProductRowMapper2 implements RowMapper<BuyProductDTO> {
 		BuyProductDTO data = new BuyProductDTO();
 		data.setSerialPK(rs.getInt("SERIAL_PK"));
 		data.setSerialRegdate(rs.getDate("SERIAL_REGDATE"));
+		data.setBuyProductPK(rs.getInt("BUYPRODUCT_PK"));
+		data.setBuyProductCnt(rs.getInt("BUYPRODUCT_CNT"));
+		data.setProductPK(rs.getInt("PRODUCT_PK"));
 		data.setProductName(rs.getString("PRODUCT_NAME"));
 		data.setProductPrice(rs.getInt("PRODUCT_PRICE"));
 		data.setProductImg(rs.getString("PRODUCT_IMG"));
-		data.setProductPK(rs.getInt("PRODUCT_PK"));
-		data.setBuyProductCnt(rs.getInt("BUYPRODUCT_CNT"));
 		return data;
 	}
 }
