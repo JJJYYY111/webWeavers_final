c23012e8066c1a86946286f6acdfab4fd4b656aa
hoihoigd <hoihoigd@naver.com>
2024-03-21 11:04:34 +0900

    [update] Serial 주문검색쿼리 추가

--- src/main/java/com/webWeavers/weaveGlow/biz/serial/SerialDAO.java
+++ src/main/java/com/webWeavers/weaveGlow/biz/serial/SerialDAO.java
@@ -17,27 +17,54 @@ public class SerialDAO {
 	
 	// 전체 주문현황_(관리자)주문관리페이지
 	private static final String SELECTALL_ORDERLIST = "SELECT\r\n"
-			+ "	S.SERIAL_PK, M.MEMBER_ID, M.MEMBER_NAME,"
-			+ " B.BUYPRODUCT_PK, B.BUYPRODUCT_STATUS, S.SERIAL_REGDATE, S.SERIAL_DELIVERYADDRESS,\r\n"
+			+ "	S.SERIAL_PK, S.SERIAL_REGDATE, S.SERIAL_DELIVERYADDRESS,\r\n"
+			+ "	M.MEMBER_ID, M.MEMBER_NAME, B.BUYPRODUCT_PK, B.BUYPRODUCT_STATUS,\r\n"
 			+ "	P.PRODUCT_PK, P.PRODUCT_NAME, P.PRODUCT_STATUS,\r\n"
-			+ "	P.PRODUCT_PRICE, B.BUYPRODUCT_CNT, P.PRODUCT_PRICE * B.BUYPRODUCT_CNT AS TOTALPRICE\r\n"
+			+ "	P.PRODUCT_PRICE, B.BUYPRODUCT_CNT,\r\n"
+			+ "	P.PRODUCT_PRICE * B.BUYPRODUCT_CNT AS TOTALPRICE\r\n"
 			+ "FROM SERIAL S\r\n"
-			+ "INNER JOIN BUYPRODUCT B ON S.SERIAL_PK = B.SERIAL_PK\r\n"
-			+ "LEFT JOIN PRODUCT P ON B.PRODUCT_PK = P.PRODUCT_PK\r\n"
-			+ "LEFT JOIN MEMBER M ON S.MEMBER_ID = M.MEMBER_ID\r\n"
+			+ "INNER JOIN BUYPRODUCT B ON\r\n"
+			+ "	S.SERIAL_PK = B.SERIAL_PK\r\n"
+			+ "LEFT JOIN PRODUCT P ON\r\n"
+			+ "	B.PRODUCT_PK = P.PRODUCT_PK\r\n"
+			+ "LEFT JOIN MEMBER M ON\r\n"
+			+ "	S.MEMBER_ID = M.MEMBER_ID\r\n"
 			+ "ORDER BY S.SERIAL_PK DESC";
 	// 주문번호별 상품현황_(관리자)주문관리페이지
 	private static final String SELECTALL_ORDERPRODUCT = "SELECT\r\n"
-			+ "	S.SERIAL_PK, M.MEMBER_ID, M.MEMBER_NAME,"
-			+ " B.BUYPRODUCT_PK, B.BUYPRODUCT_STATUS, S.SERIAL_REGDATE, S.SERIAL_DELIVERYADDRESS,\r\n"
+			+ "	S.SERIAL_PK, S.SERIAL_REGDATE, S.SERIAL_DELIVERYADDRESS,\r\n"
+			+ "	M.MEMBER_ID, M.MEMBER_NAME, B.BUYPRODUCT_PK, B.BUYPRODUCT_STATUS,\r\n"
 			+ "	P.PRODUCT_PK, P.PRODUCT_NAME, P.PRODUCT_STATUS,\r\n"
-			+ "	P.PRODUCT_PRICE, B.BUYPRODUCT_CNT, P.PRODUCT_PRICE * B.BUYPRODUCT_CNT AS TOTALPRICE\r\n"
+			+ "	P.PRODUCT_PRICE, B.BUYPRODUCT_CNT,\r\n"
+			+ "	P.PRODUCT_PRICE * B.BUYPRODUCT_CNT AS TOTALPRICE\r\n"
 			+ "FROM SERIAL S\r\n"
-			+ "INNER JOIN BUYPRODUCT B ON S.SERIAL_PK = B.SERIAL_PK\r\n"
-			+ "LEFT JOIN PRODUCT P ON B.PRODUCT_PK = P.PRODUCT_PK\r\n"
-			+ "LEFT JOIN MEMBER M ON S.MEMBER_ID = M.MEMBER_ID\r\n"
-			+ "WHERE S.SERIAL_PK = ? \r\n"
+			+ "INNER JOIN BUYPRODUCT B ON\r\n"
+			+ "	S.SERIAL_PK = B.SERIAL_PK\r\n"
+			+ "LEFT JOIN PRODUCT P ON\r\n"
+			+ "	B.PRODUCT_PK = P.PRODUCT_PK\r\n"
+			+ "LEFT JOIN MEMBER M ON\r\n"
+			+ "	S.MEMBER_ID = M.MEMBER_ID\r\n"
+			+ "WHERE S.SERIAL_PK = ?\r\n"
 			+ "ORDER BY S.SERIAL_PK DESC";
+	// 주문검색_(관리자)주문관리페이지
+	private static final String SELECTALL_ORDERSEARCH = "SELECT\r\n"
+			+ "	S.SERIAL_PK, S.SERIAL_REGDATE, S.SERIAL_DELIVERYADDRESS,\r\n"
+			+ "	M.MEMBER_ID, M.MEMBER_NAME, B.BUYPRODUCT_PK, B.BUYPRODUCT_STATUS,\r\n"
+			+ "	P.PRODUCT_PK, P.PRODUCT_NAME, P.PRODUCT_STATUS,\r\n"
+			+ "	P.PRODUCT_PRICE, B.BUYPRODUCT_CNT,\r\n"
+			+ "	P.PRODUCT_PRICE * B.BUYPRODUCT_CNT AS TOTALPRICE\r\n"
+			+ "FROM SERIAL S\r\n"
+			+ "INNER JOIN BUYPRODUCT B ON\r\n"
+			+ "	S.SERIAL_PK = B.SERIAL_PK\r\n"
+			+ "LEFT JOIN PRODUCT P ON\r\n"
+			+ "	B.PRODUCT_PK = P.PRODUCT_PK\r\n"
+			+ "LEFT JOIN MEMBER M ON\r\n"
+			+ "	S.MEMBER_ID = M.MEMBER_ID\r\n"
+			+ "WHERE\r\n"
+			+ "	B.BUYPRODUCT_STATUS = ?\r\n"
+			+ "	AND LOWER(S.MEMBER_ID) LIKE '%'||?||'%'\r\n"
+			+ "	AND M.MEMBER_NAME LIKE '%'||?||'%'\r\n"
+			+ "ORDER BY B.BUYPRODUCT_PK DESC";
 //	private static final String SELECTONE = "";
 	// 주문추가_(관리자)주문관리페이지
 	private static final String INSERT = "INSERT INTO SERIAL (MEMBER_ID, SERIAL_DELIVERYADDRESS) VALUES (?, ?)";
@@ -45,12 +72,15 @@ public class SerialDAO {
 //	private static final String DELETE = "";
 
 	public List<SerialDTO> selectAll(SerialDTO serialDTO) {
-		Object args[] = { serialDTO.getSerialPK() };
+		Object args1[] = { serialDTO.getSerialPK() };
+		Object args2[] = { serialDTO.getBuyProductStatus(), serialDTO.getMemberID(), serialDTO.getMemberName() };
 		try {
-			if(serialDTO.getSearchCondition().equals("orderList"))
-			return jdbcTemplate.query(SELECTALL_ORDERLIST, new SerialListAdminRowMapper());
-			else if(serialDTO.getSearchCondition().equals("orderProduct")) {
-				return jdbcTemplate.query(SELECTALL_ORDERPRODUCT, args, new SerialListAdminRowMapper());				
+			if (serialDTO.getSearchCondition().equals("orderList")) {
+				return jdbcTemplate.query(SELECTALL_ORDERLIST, new SerialListAdminRowMapper());
+			} else if (serialDTO.getSearchCondition().equals("orderProduct")) {
+				return jdbcTemplate.query(SELECTALL_ORDERPRODUCT, args1, new SerialListAdminRowMapper());
+			} else if (serialDTO.getSearchCondition().equals("orderSearch")) {
+				return jdbcTemplate.query(SELECTALL_ORDERSEARCH, args2, new SerialListAdminRowMapper());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
