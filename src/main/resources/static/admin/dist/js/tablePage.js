eb7ede7eb0ecfbe9378a2017fe4ee5348580674f
suyeon <sooyeon0119@naver.com>
2024-04-01 13:26:42 +0900

    [update] 페이징 처리 수정

--- src/main/resources/static/admin/dist/js/tablePage.js
+++ src/main/resources/static/admin/dist/js/tablePage.js
@@ -26,71 +26,92 @@ function createPagination($table, navId, rowPerPage) {
     var pageTotal = Math.ceil(rowTotals / rowPerPage);
     var i = 0;
 
-    $('<a href="#" class="arrow prev"><span>&lt;</span></a>').appendTo('#' + navId);
-
     var currentPage = 0; // 현재 페이지 번호 설정
-    var startPage = currentPage * rowPerPage; // 시작 항목 인덱스 설정
-    var endPage = Math.min((currentPage + 1) * rowPerPage, rowTotals); // 종료 항목 인덱스 설정
 
-    for (i = 0; i < pageTotal; i++) { // 페이지 버튼을 생성
+    // 페이지 버튼 이벤트 처리
+    function handlePageClick(targetPage) {
+        currentPage = targetPage;
+        var startPage = currentPage * rowPerPage;
+        var endPage = Math.min((currentPage + 1) * rowPerPage - 1, rowTotals - 1); // 종료 항목 인덱스 설정
+
+        $tr.addClass('off-screen').slice(startPage, endPage + 1).removeClass('off-screen');
+
+        $('#' + navId + ' .page').removeClass('active'); // 모든 페이지 버튼의 활성 클래스 제거
+        $('#' + navId + ' .page[rel=' + currentPage + ']').addClass('active'); // 클릭된 페이지 버튼에만 활성 클래스 추가
+    }
+// 페이지 버튼 생성
+function generatePageButtons(start, end) {
+    $('#' + navId).empty(); // 기존 페이지 버튼 제거
+    $('<a href="#" class="arrow prev"><span>&lt;</span></a>').appendTo('#' + navId); // 이전 버튼 추가
+
+    for (i = start; i <= end; i++) {
         $('<a href="#" class="page"></a>')
             .attr('rel', i)
             .html(i + 1)
-            .appendTo('#' + navId);
+            .appendTo('#' + navId)
+            .click(function() {
+                handlePageClick(parseInt($(this).attr('rel'))); // 페이지 번호는 0부터 시작하지 않으므로 변경 없이 사용
+            });
     }
 
-    $('<a href="#" class="arrow next"><span>&gt;</span></a>').appendTo('#' + navId);
+    $('<a href="#" class="arrow next"><span>&gt;</span></a>').appendTo('#' + navId); // 다음 버튼 추가
 
- $tr.addClass('off-screen')
+    // 초기 활성화된 페이지 설정
+    $('#' + navId + ' .page[rel=' + currentPage + ']').addClass('active');
+
+    // "이전" 버튼 클릭 이벤트 처리
+    $('#' + navId + ' .arrow.prev').click(function(evt) {
+        evt.preventDefault();
+        if (currentPage > 0) {
+            handlePageClick(currentPage - 1);
+        }
+    });
+}
+
+    // 초기 페이지 버튼 생성
+    generatePageButtons(0, Math.min(pageTotal - 1, 9));
+
+    $tr.addClass('off-screen')
         .slice(0, rowPerPage)
         .removeClass('off-screen');
 
+// 다음 화살표 이벤트 처리
+$(document).on('click', '#' + navId + ' .arrow.next', function(evt) {
+    evt.preventDefault();
+    var nextPageGroupStart = Math.floor((currentPage + 1) / 10) * 10; // 다음 페이지 그룹의 시작 페이지
+    var nextPageGroupEnd = Math.min(nextPageGroupStart + 9, pageTotal - 1); // 다음 페이지 그룹의 끝 페이지
 
-    // 페이지 버튼 이벤트 처리
-    var $pagingLink = $('#' + navId + ' .page');
-    $pagingLink.on('click', function(evt) {
-        evt.preventDefault();
-        var $this = $(this);
-        if ($this.hasClass('active')) {
-            return;
-        }
-        $pagingLink.removeClass('active');
-        $this.addClass('active');
+    if (currentPage < pageTotal - 1 && currentPage < nextPageGroupStart) {
+        var nextPage = nextPageGroupStart;
+        var startPage = nextPage;
+        var endPage = Math.min(startPage + 9, pageTotal - 1);
 
-        currentPage = parseInt($this.attr('rel'));
-        startPage = currentPage * rowPerPage;
-        endPage = Math.min((currentPage + 1) * rowPerPage, rowTotals);
+        // 페이지 버튼 재생성
+        generatePageButtons(startPage, endPage);
 
-        $tr.removeClass('off-screen').slice(0, startPage).addClass('off-screen');
-        $tr.slice(startPage, endPage).removeClass('off-screen');
-        $tr.slice(endPage).addClass('off-screen');
-    });
+        handlePageClick(nextPage);
+    } else if (currentPage < pageTotal - 1) {
+        handlePageClick(currentPage + 1);
+    }
+});
 
-    // 이전/다음 화살표 이벤트 처리
-    $('#' + navId + ' .arrow').on('click', function(evt) {
-        evt.preventDefault();
-        var $this = $(this);
-        var $activePage = $('#' + navId + ' .page.active');
+// 이전 화살표 이벤트 처리
+$(document).on('click', '#' + navId + ' .arrow.prev', function(evt) {
+    evt.preventDefault();
 
-        if ($this.hasClass('prev')) {
-            if (currentPage > 0) {
-                currentPage--;
-            }
-        } else if ($this.hasClass('next')) {
-            if (currentPage < pageTotal - 1) {
-                currentPage++;
-            }
-        }
+    if (currentPage > 0) {
+        var prevPage = currentPage - 0; // 현재 페이지 번호에서 1을 빼서 이전 페이지로 이동
+        var startPage = Math.floor(prevPage / 10) * 10; // 이전 페이지 그룹의 시작 페이지
+        var endPage = Math.min(startPage + 9, pageTotal - 1); // 이전 페이지 그룹의 끝 페이지
 
-        $('#' + navId + ' .page[rel=' + currentPage + ']').click();
-    });
+        // 페이지 버튼 재생성
+        generatePageButtons(startPage, endPage);
+
+        handlePageClick(prevPage);
+    }
+});
 
     // 초기 활성화된 페이지 설정
-    $pagingLink.filter('[rel=' + currentPage + ']').addClass('active');
+    $('#' + navId + ' .page[rel=' + currentPage + ']').addClass('active');
 }
-
-
-
-
-
 
