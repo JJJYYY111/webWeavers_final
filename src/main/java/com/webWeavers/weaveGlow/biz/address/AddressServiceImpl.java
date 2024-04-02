bca14a1d37828e05b5422d2d0c5a0a56a2f3900c
leesin <a25611640@gmail.com>
2024-04-02 13:37:47 +0900

    [ UPDATE ] mybatis, AddressDAO.xml, SerialDAO.xml 설정 변경

--- src/main/java/com/webWeavers/weaveGlow/biz/address/AddressServiceImpl.java
+++ src/main/java/com/webWeavers/weaveGlow/biz/address/AddressServiceImpl.java
@@ -15,7 +15,9 @@ public class AddressServiceImpl implements AddressService {
 
 	@Override
 	public List<AddressDTO> selectAll(AddressDTO addressDTO) {
-		 return addressDAO.selectAll(addressDTO.getMemberID());
+		Map<String,String>map = new HashMap<String,String>();
+		map.put("memberID", addressDTO.getMemberID());
+		 return addressDAO.selectAll(map);
 	}
 
 	@Override
@@ -27,12 +29,26 @@ public class AddressServiceImpl implements AddressService {
 
 	@Override
 	public boolean insert(AddressDTO addressDTO) {
-		return addressDAO.insert(addressDTO);
+		Map<String, Object> map = new HashMap<String,Object>();
+		map.put("memberID", addressDTO.getMemberID());
+		map.put("addressZonecode", addressDTO.getAddressZonecode());
+		map.put("addressJibun", addressDTO.getAddressJibun());
+		map.put("addressRoad", addressDTO.getAddressRoad());
+		map.put("addressDetail", addressDTO.getAddressDetail());
+		map.put("addressName", addressDTO.getAddressName());
+		return addressDAO.insert(map);
 	}
 
 	@Override
 	public boolean update(AddressDTO addressDTO) {
-		 return addressDAO.update(addressDTO);
+		Map<String, Object> map = new HashMap<String,Object>();
+		map.put("addressZonecode", addressDTO.getAddressZonecode());
+		map.put("addressJibun", addressDTO.getAddressJibun());
+		map.put("addressRoad", addressDTO.getAddressRoad());
+		map.put("addressDetail", addressDTO.getAddressDetail());
+		map.put("addressName", addressDTO.getAddressName());
+		map.put("addressPK", addressDTO.getAddressPK());
+		return addressDAO.update(map);
 	}
 
 	@Override
