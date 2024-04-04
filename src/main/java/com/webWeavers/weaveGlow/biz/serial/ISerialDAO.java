974895c473b62db771b028df9968014f9a964775
hoihoigd <hoihoigd@naver.com>
2024-04-04 14:20:40 +0900

    [ update ] 수상한 mybatis Serial

--- src/main/java/com/webWeavers/weaveGlow/biz/serial/ISerialDAO.java
+++ src/main/java/com/webWeavers/weaveGlow/biz/serial/ISerialDAO.java
@@ -7,13 +7,22 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ISerialDAO {
+	
+	// 전체 주문현황_(관리자)주문관리페이지
+	public List<SerialDTO> selectAllOrderList();
+	
+	// 주문번호별 상품현황_(관리자)주문관리페이지
+	public List<SerialDTO> selectAllOrderProduct(Map<String, Object> map);
+	
+	// 주문검색_(관리자)주문관리페이지	
+	public List<SerialDTO> selectAllOrderSearch(Map<String, Object> map);
 
-	public List<SerialDTO> selectAll();
-	
+	// 주문추가_(관리자)주문관리페이지	
 	// 정석
-	public boolean insert(String memberID, String serialDeliveryAddress);
+	public int insert(String memberID, String serialDeliveryAddress);
 	
+	// 주문상태변경_(관리자)주문관리페이지	
 	// 파라미터는 SQL문이 변경될때마다 파라미터도 달질 수 있음 > 결합도를 낮추는 Map을 인자로 주는 경우
-	public boolean update(Map<String, Object> map);	
+	public int update(Map<String, Object> map);	
 	
 }
--- src/main/java/com/webWeavers/weaveGlow/biz/serial/SerialServiceImpl.java
+++ src/main/java/com/webWeavers/weaveGlow/biz/serial/SerialServiceImpl.java
@@ -1,6 +1,8 @@
 package com.webWeavers.weaveGlow.biz.serial;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,26 @@ import org.springframework.stereotype.Service;
 public class SerialServiceImpl implements SerialService {
 
 	@Autowired
-	private SerialDAO serialDAO;
+	private ISerialDAO serialDAO;
 
 	@Override
 	public List<SerialDTO> selectAll(SerialDTO serialDTO) {
-		return serialDAO.selectAll(serialDTO);
+		if (serialDTO.getSearchCondition().equals("orderList")) {
+			return serialDAO.selectAllOrderList();
+		}
+		else if (serialDTO.getSearchCondition().equals("orderProduct")) {
+			Map<String, Object> map = new HashMap<String, Object>();	
+			map.put("serialPK", serialDTO.getSerialPK());
+			return serialDAO.selectAllOrderProduct(map);
+		}
+		else if (serialDTO.getSearchCondition().equals("orderSearch")) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("serialStatus", serialDTO.getSerialStatus());
+			map.put("memberName", serialDTO.getMemberName());
+			map.put("serialRegdate", serialDTO.getSerialRegdate());
+			return serialDAO.selectAllOrderSearch(map);
+		}
+		return null;
 	}
 
 //	@Override
@@ -21,19 +38,31 @@ public class SerialServiceImpl implements SerialService {
 //		return serialDAO.selectOne(serialDTO);
 //	}
 
+	// 정석
 	@Override
 	public boolean insert(SerialDTO serialDTO) {
-		return serialDAO.insert(serialDTO);
+		int result = serialDAO.insert(serialDTO.getMemberID(), serialDTO.getSerialDeliveryAddress());
+		if (result <= 0) {
+			return false;
+		}
+		return true;
 	}
 
+	// 파라미터는 SQL문이 변경될때마다 파라미터도 달질 수 있음 > 결합도를 낮추는 Map을 인자로 주는 경우
 	@Override
 	public boolean update(SerialDTO serialDTO) {
-		return serialDAO.update(serialDTO);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("serialStatus", serialDTO.getSerialStatus());
+		map.put("serialPK", serialDTO.getSerialPK());
+		if (serialDAO.update(map) <= 0) {
+			return false;
+		}
+		return true;
 	}
 
 //	@Override
 //	public boolean delete(SerialDTO serialDTO) {
-//		return serialDAO.delete(serialDTO);
+//		return false;
 //	}
 
 }
